20420c964d9ce3bd6edbc65d93a2900c304aea82
jslingsby <jslingsby@gmail.com>
2016-05-04 14:46:55 +0200

    Tweaks

--- Code/DataPrep_Test_DoNotEdit.R
+++ Code/DataPrep_Test_DoNotEdit.R
@@ -3,7 +3,7 @@
 ######## and protected area data for VegMapTools analyses
 ##########################################
 ######## Compiled by Jasper Slingsby 2016
-######## Last edited: 14 April 2016
+######## Last edited: 12 April 2016
 ##########################################
 
 ##########################################
@@ -17,89 +17,12 @@ library(dplyr)
 if(Sys.getenv("USER")=="jasper") {datwd <- "/Users/jasper/Documents/GIS/VegToolsRaw/"}
 if(Sys.getenv("USERNAME")=="MatlalaM") {datwd <- "Your data directory"}
 
-#my_db <- src_sqlite("Data/VMT.sqlite3", create = T)
-
 ##########################################
-###3) Get and process data
+###2) Get and process data
 ##########################################
-
-## Set extent
-#nex <- extent(2020000, 2140000, -4100000, -3950000) #CoCT
-nex <- extent(2030000, 2060000, -4080000, -4010000) #Peninsula?
-
-#VT <- stack(c("/Users/jasper/Documents/GIS/VegToolsRaw/Rasters/LC13test_web.tif", "/Users/jasper/Documents/GIS/VegToolsRaw/Rasters/PA_npaes_test_web.tif", "/Users/jasper/Documents/GIS/VegToolsRaw/Rasters/VEG12test_web.tif"))
-
-#Land cover
-#lc <- raster(paste(datwd, "Landcover/LCsmiso_trans1.tif", sep=""))
-lc <- raster("/Users/jasper/Documents/GIS/VegToolsRaw/Rasters/LC13test_web.tif")
-proj4string(lc) <- CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs")
-NAvalue(lc) <- 128
-
-#Protected areas
-pa <- raster("/Users/jasper/Documents/GIS/VegToolsRaw/Rasters/PA_npaes_test_web.tif")
-proj4string(pa) <- CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs")
-
-#Vegetation map
-vm <- raster("/Users/jasper/Documents/GIS/VegToolsRaw/Rasters/VEG12test_web.tif")
-proj4string(vm) <- CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs")
-
-
-#Check if rasters match in extent, number of rows and columns, projection, resolution, and origin
-compareRaster(lc, pa, vm)
-
-#Crop to extent
-lc <- crop(lc, nex)
-pa <- crop(pa, nex)
-vm <- crop(vm, nex)
-
-lcx <- lc
-
-
-
-y <- levels(lc)[[1]]$Value[which(levels(lc)[[1]]$LC3V==1)]
-lc2 <- projectExtent(lc, crs = CRS(proj4string(lc)))
-
-x <- getValues(lc)
-x[which(x%in%y)] <- 1
-x[-which(x%in%y)] <- 0
-lc2 <- setValues(lc2, x)
-  
-lc3 <- subs(lc, , by = "ID", which = "LC3V") #
-
-
-
-#Select minimum required data
-
-if(!exists("Data/lc2class.grd"))
-{
-lcx <- levels(lc)[[1]]
-lc3 <- subs(lc, lcx, by = "ID", which = "LC3V", filename = "Data/lc2class.grd")
-}
-
-lc3 <- raster("Data/lc2class.grd")
-
-
-
-###
-
-lc3 <- deratify(lc, att="LC3V", complete=TRUE, drop=TRUE) 
-pa2 <- deratify(pa, att="TYPE2", layer=1, complete=TRUE, drop=TRUE) 
-
-all <- stack(vm,lc,pa)
-
-names(all) <- c("VM", "LC", "PA")
-
-writeRaster(all, "Data/VMT.tif")
-
-#Make a data table
-dat <- data.frame(LandCover = getValues(lc), ProtectedAreas = getValues(pa), VegTypes = getValues(vm))
-
-
-
-EPSG3857
 
 ###Set desired extent
-nex <- extent(-320000, -220000, 6150000, 6250000) #-64029.64, -45589.64, 3749978, 3803538
+nex <- extent(-320000, -220000, 6150000, 6250000)
 
 ###Land cover - get, crop and write out, read in
 lc <- raster(paste(datwd, "Landcover/LCsmiso_trans1.tif", sep=""))
@@ -122,7 +45,7 @@ vm2006 <- crop(vm2006, nex) #crop
 
 rasterize(vm2006, lc, filename="Data/vegtype.grd", overwrite=T) #Rasterize  #, field="NAME"
 vt <- raster("Data/vegtype.grd") #Read in rasterized veg types
-#vt <- as.factor(vt)
+vtnames <- levels(vt)[[1]]
 #vtnames <- levels(vm2006$NAME)[levels(vt)[[1]]$ID]
 #levels(vt) <- data.frame(ID = levels(vt)[[1]]$ID, NAMES = vtnames)
 rm(vm2006) #Remove any large objects that aren't being used from memory...
@@ -155,68 +78,94 @@ rasterize(pas, lc, filename="Data/protected.grd", overwrite=T) #rasterize
 pas <- raster("Data/protected.grd") #read in raster
 
 ###Make a data table
-#trainingbrick <- brick(vt, lc, clumps)
-dat <- data.frame(LandCover = getValues(lc), VegType = getValues(vt), Fragment = getValues(clumps), Protected = getValues(pas))
+
+#One way to make a table - slower, but this way checks that the extent, coordinate reference system and resolution are the same
+trainingbrick <- brick(vt, lc, clumps, pas)
+names(trainingbrick) <- c("VegType", "LandCover", "Fragment", "Protected")
+tb <- data.frame(getValues(trainingbrick))
+
+#Another way, but this just checks that they have the same number of cells (!!!NB!!! It does not check extent, resolution or CRS)
+dat <- data.frame(VegType = getValues(vt), LandCover = getValues(lc), Fragment = getValues(clumps), Protected = getValues(pas))
+
+#Check that they are the same
+identical(tb, dat) #Not the same!!! But why? - Let's look at the objects more closely
+
+class(tb) #Are they the same class?
+class(dat)
+sapply(dat, class) #Are the columns in each data object the same class?
+sapply(tb, class) #NO!? 
+
+dat$LandCover <- as.numeric(dat$LandCover) #Let's make those integer columns numeric
+dat$Fragment <- as.numeric(dat$Fragment)
+
+identical(tb, dat) #Yay!!!
+
+rm(tb) #Remove one of the tables from memory - to help R think
+
 dat <- dat[!is.na(dat$VegType),] #Remove any pixels that dont have a veg type...
 dat$LandCover[which(dat$LandCover==2)] <- 0 #set landcover data so that only "Natural" has 1 and all else is NA or 0
-dat$VegType <- as.factor(dat$VegType)
-x<-levels(vt)
-levels(dat$VegType) <- levels(x[[1]]$NAME) #vtnames
 
+dat$VegType <- vtnames$NAME[dat$VegType]
+levels(dat$VegType)
+
+#Make dat a special table that library(dplyr) works with
 dtbl <- tbl_df(dat)
 
-###Calculate veg type extents
+###Calculate veg type extents - using dplyr queries
 original <- summarise(group_by(dtbl, VegType), original = (length(LandCover)*900)/10000)
 remaining <- summarise(group_by(dtbl, VegType), remaining = (sum(LandCover)*900)/10000)
 protected <- summarise(group_by(filter(dtbl, Protected>0) , VegType), protected = (sum(LandCover)*900)/10000)
+
+
 #fragments? - max size, # clumps, etc?
 
 ###Join extents and calculate threat status and protection level
 d <- left_join(original, remaining, by="VegType")
 d <- left_join(d, protected, by="VegType")
+d$protected[is.na(d$protected)] <-0 #Make the veg types that are NA (i.e. have no protected cells) 0
 d$perc_remaining <- 100*d$remaining/d$original
 d$perc_protected <- 100*d$protected/d$original
 d$threat_status <- cut(d$perc_remaining, breaks=c(0,20,35,60,100), labels = c("CR", "EN", "VU", "LC"))
-d$protection_level <- cut(d$perc_protected, c(0,4,10,20,100), labels = c("Hardly", "Poorly", "Moderately", "Well"))
+d$protection_level <- cut(d$perc_protected, c(-1,4,10,20,100), labels = c("Hardly", "Poorly", "Moderately", "Well"))
 
 ########END########
 
 ###Elevation
 # Mosaic the 90m SRTM digital elevation model for South Africa
 system.time( #Time the process
-  if(!file.exists("Data/dem_SRTM90.tif")) {
-    mosaic_rasters(list.files("/Users/jasper/Documents/GIS/SRTM/All", full.names=T, pattern = ".tif"), "Data/dem_SRTM90.tif", output.vrt = NULL, output_Raster = TRUE, trim_margins = NULL, verbose = FALSE)
-  } else {dem <- raster("Data/dem_SRTM90.tif")}
+if(!file.exists("Data/dem_SRTM90.tif")) {
+mosaic_rasters(list.files("/Users/jasper/Documents/GIS/SRTM/All", full.names=T, pattern = ".tif"), "Data/dem_SRTM90.tif", output.vrt = NULL, output_Raster = TRUE, trim_margins = NULL, verbose = FALSE)
+} else {dem <- raster("Data/dem_SRTM90.tif")}
 )
 
 ###Climate
 # Get "current" climate data from Schulze et al 2007 - vars include map, mmp01, pptconc, tmax01, tmin07
 if(!file.exists("Data/currentclimate.grd")) {
-  
-  grids <- c("/Users/jasper/Documents/GIS/Schultze (2007)/grids/gmap", "/Users/jasper/Documents/GIS/Schultze (2007)/grids/gmednrfl1", "/Users/jasper/Documents/GIS/Schultze (2007)/grids/tmaxave01c", "/Users/jasper/Documents/GIS/Schultze (2007)/grids/tminave07c") #The paths to the files we need
-  gridsn <- sapply(grids, FUN = function(x){strsplit(x, "/")[[1]][8]}) #Extract the file names
+
+grids <- c("/Users/jasper/Documents/GIS/Schultze (2007)/grids/gmap", "/Users/jasper/Documents/GIS/Schultze (2007)/grids/gmednrfl1", "/Users/jasper/Documents/GIS/Schultze (2007)/grids/tmaxave01c", "/Users/jasper/Documents/GIS/Schultze (2007)/grids/tminave07c") #The paths to the files we need
+gridsn <- sapply(grids, FUN = function(x){strsplit(x, "/")[[1]][8]}) #Extract the file names
   
   #Align all rasters to the same extent, grid, projection...
   if(!file.exists("Data/gmap.tif")) {
-    for (i in 1:length(grids))
+  for (i in 1:length(grids))
     {
       align_rasters(grids[i], grids[1], paste("Data/", gridsn[i], ".tif", sep=""), output_Raster = TRUE, nThreads = 2, verbose = FALSE)
     }
   } else {clim <- stack(paste("Data/", gridsn, ".tif", sep=""))}
-  
+
   #Get and rasterize pptcon to the same grid
   if(!file.exists("Data/pptcon.grd")) {
-    pptcon <- readOGR(dsn = "/Users/jasper/Documents/GIS/Schultze (2007)/shape_files/rfl_seasconc.shp", layer = "rfl_seasconc")
-    rasterize(pptcon, clim, field = "RFL_CONC", filename = "Data/pptcon.grd")} else {
-      pptcon <- raster("Data/pptcon.grd")
-    }
-  
+  pptcon <- readOGR(dsn = "/Users/jasper/Documents/GIS/Schultze (2007)/shape_files/rfl_seasconc.shp", layer = "rfl_seasconc")
+  rasterize(pptcon, clim, field = "RFL_CONC", filename = "Data/pptcon.grd")} else {
+  pptcon <- raster("Data/pptcon.grd")
+  }
+
   #Bind the aligned rasters together, rename and write to file
   clim <- stack(clim, pptcon)
   names(clim) <- c("map", "mmp01", "tmax01", "tmin07", "pptconc") #Use names that match the futures names
   writeRaster(clim, "Data/currentclimate.grd")
 } else {
-  clim <- brick("Data/currentclimate.grd") #Otherwise, if the file does exist, just read it in as a RasterBrick
+clim <- brick("Data/currentclimate.grd") #Otherwise, if the file does exist, just read it in as a RasterBrick
 }
 #rm(clim, grids, gridsn, pptcon)
 
@@ -238,7 +187,7 @@ futureInfo$Variable <- sapply(futureInfo$Variable, function(x) {strsplit(x, ".",
 
 for (i in 1:nlayers(clim))
 {
-  fclim[[which(futureInfo$Variable==names(clim)[i])]] <- clim[[i]] + fclim[[which(futureInfo$Variable==names(clim)[i])]]
+fclim[[which(futureInfo$Variable==names(clim)[i])]] <- clim[[i]] + fclim[[which(futureInfo$Variable==names(clim)[i])]]
 }
 
 # Get veg age/return interval data (including projections from multiple models)
